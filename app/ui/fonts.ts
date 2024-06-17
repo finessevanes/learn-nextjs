deca6ff28cd4e07c92e94a2ed79641cd76f5d000
Vanessa Mercado <vanessa.mercado24@gmail.com>
2024-06-17 16:41:46 -0700

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,14 @@
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
+
+
+// Now it's your turn! In your fonts.ts file, import a secondary font called Lusitana and pass it to the <p> element in your /app/page.tsx file. In addition to specifying a subset like you did before, you'll also need to specify the font weight.
+
+// Once you're ready, expand the code snippet below to see the solution.
+
